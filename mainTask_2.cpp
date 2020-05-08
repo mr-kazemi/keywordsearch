7e7d5b15ffa3259b3d1482532138c58cf89e78ff
unknown <ztalebi7676@gmail.com>
2020-05-08 19:51:39 +0430

    longdouble_to_double

--- mainTask_2.cpp
+++ mainTask_2.cpp
@@ -1,19 +1,11 @@
 #include <bits/stdc++.h>
-
+#include "point.h"
 using namespace std;
 #define pll pair<long long,long long>
-struct Point{
-    long double x,y;
-    int color;
-    Point(long double _x=0,long double _y=0,int _color=0){
-        x=_x;
-        y=_y;
-        color=_color;
-    }
-};
-map<pll,set<int> > task_2(vector<Point> point,long double delta,long double epsilon){
+
+map<pll,set<int> > task_2(vector<Point> point,double delta,double epsilon){
     map<pll,set<int> > table;
-    long double a=(epsilon)/3;
+    double a=(epsilon)/3;
     a*=delta;
     long long help_x,help_y;
     for(int i=0;i<(int)point.size();i++){
@@ -30,7 +22,7 @@ int main()
     Point d=Point(0.49,0.001,1);
     c.push_back(d);
     map<pll,set<int> > a=task_2(c,3,0.11);
-    // cout<<a[pdd(0.44,0)].size()<<endl;
+    //cout<<a[pll(0.44,0)].size()<<endl;
     //set<int> b=a[pdd(1.33,2)];
     /*for(set<int>::iterator it=b.begin();it!=b.end();++it){
         cout<<*it<<endl;
--- point.h
+++ point.h
@@ -1,7 +1,7 @@
 struct Point{
-    long double x,y;
+    double x,y;
     int color;
-    Point(long double _x=0,long double _y=0,int _color=0){
+    Point(double _x=0,double _y=0,int _color=0){
         x=_x;
         y=_y;
         color=_color;
