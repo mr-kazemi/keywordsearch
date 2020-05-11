c84e5fef0d709879936f9217300fb4d98f9e6933
Parisa Shamsi <pari.p.shamsi.s@gmail.com>
2020-05-11 15:51:24 +0430

    task 4 _ second update

--- Task4.cpp
+++ Task4.cpp
@@ -5,69 +5,60 @@ using namespace std;
 
 #define pll pair<long long,long long>
 
-struct Point{
-    long double x,y;
-    int color;
-    Point(long double _x=0,long double _y=0,int _color=0){
-        x=_x;
-        y=_y;
-        color=_color;
-    }
-};
+double diameter(vector<pll> points, float gridSize, int number){
 
-map<pll,set<int>> grid{ // output of another task! it's just a sample.
-    {pll(0, 2), {1, 2}},
-	{pll(3, 1), {2}},
-	{pll(3, 5), {1}},
-	{pll(5, 5), {1}}
-};
+	//find left most and right most in each row
+	vector<long long> left;
+	vector<long long> right;
+	for(int i=0; i<number; i++){
+		left.push_back(-1);
+		right.push_back(-1);
+	}
+	
+	for(int i=0; i<points.size(); i++){
+		// find left in every row
+		if(left[points[i].first] == -1) {
+			left[points[i].first] = points[i].second;
+		}
+		else if(left[points[i].first] > points[i].second) {
+			left[points[i].first] = points[i].second;
+		}
 
-double diameter(int dimensions){
-	map<pll,set<int>>::iterator itr; 
-    double diam = 0; // min possible
-    for(itr = grid.begin(); itr != grid.end(); itr++) { // points
-        pll point = itr->first;
-        for(int row=point.first; row <= dimensions; row++) { // lower rows
-        	for(int col = 0; col <= point.second; col++){
-        		if(grid.count(pll(row, col)) > 0) { // find first left point in the row
-        			if (point.first == row && point.second == col) { // same point
-        				break;
-        			}
-	        		double dist = sqrt(pow((point.first - row), 2) + pow((point.second - col), 2)); // distance between two points
-	        		if (diam < dist) { // update diameter
-	        			diam = dist;
-	        			//cout << "d: " << diam << endl;
-	        			//cout << "first: " << point.first << "\t second: " << point.second << endl;
-	        			//cout << "row: " << row << "\t col: " << col << endl;
-	        		}
-	        		break;
-	        	}
-        	}
-        	for(int col = dimensions; col >= point.second; col--){
-        		if(grid.count(pll(row, col)) > 0) { // find first right point in the row
-        			if (point.first == row && point.second == col) { // same point
-        				break;
-        			}
-	        		double dist = sqrt(pow((point.first - row), 2) + pow((point.second - col), 2)); // distance between two points
-	        		if (diam < dist) { // update diameter
-	        			diam = dist;
-	        			//cout << "d: " << diam << endl;
-	        			//cout << "first: " << point.first << "\t second: " << point.second << endl;
-	        			//cout << "row: " << row << "\t col: " << col << endl;
-	        		}
-	        		break;
-	        	}
-        	}
-        }
-    } 
+		// find right in every row
+		if(right[points[i].first] == -1) {
+			right[points[i].first] = points[i].second;
+		}
+		else if(right[points[i].first] < points[i].second) {
+			right[points[i].first] = points[i].second;
+		}
+	}
+
+	// find diameter
+	double diam = 0;
+	for(int i=0; i<left.size(); i++) {
+		if(left[i] == -1) {
+			continue;
+		}
+		for(int j=0; j<right.size(); j++) {
+			if(right[j] == -1) {
+				continue;
+			}
+			double d = pow(i - j, 2) + pow(left[i] - right[j], 2);
+			if(diam < d) {
+				diam = d;
+			}
+		}
+	}
+	diam = sqrt(diam) * gridSize;
 
 	return diam;
 }
 
 int main() {
-	double epsilon = 0.1; // user's input! it's just a sample.
-	int dimensions = 1 / epsilon;
-	double diam = diameter(dimensions);
+	vector<pll> points = {pll(0, 2), pll(3, 1), pll(3, 5), pll(5, 5)};
+	float gridSize = 10;
+	int number = 5 + 1; // number of rows in grid
+	double diam = diameter(points, gridSize, number);
 	cout << "Diameter: " << diam << endl;
   	return 0;
 }
