d68273849ea72491342c4aef2c949b1405fb651a
unknown <ztalebi7676@gmail.com>
2020-05-31 23:02:30 +0430

    test

--- src/task4.cpp
+++ src/task4.cpp
@@ -80,7 +80,7 @@ int main(){
 
     int m = 12;//the number of all colors
     // just as an example for checking
-    grid[pll(1,2)].insert(1);
+    /*grid[pll(1,2)].insert(1);
     grid[pll(1,2)].insert(4);
     grid[pll(1,2)].insert(6);
     grid[pll(3,5)].insert(3);
@@ -98,13 +98,13 @@ int main(){
     grid[pll(3,4)].insert(2);
     grid[pll(5,1)].insert(4);
     grid[pll(5,1)].insert(8);
-    /*grid[pll(1,2)] = {1,4,6};
+    grid[pll(1,2)] = {1,4,6};
     grid[pll(3,5)] = {3,7,2};
     grid[pll(1,4)] = {1,5};
     grid[pll(3,6)] = {3,2};
     grid[pll(7,1)] = {5,6};
     grid[pll(3,4)] = {5,1,2};
-    grid[pll(5,1)] = {4,8};*/
+    grid[pll(5,1)] = {4,8};
     grid[pll(1,2)].insert(1);
     grid[pll(1,2)].insert(4);
     grid[pll(1,2)].insert(6);
@@ -129,20 +129,59 @@ int main(){
     grid[pll(3,6)].insert(1);
     grid[pll(3,6)].insert(2);
     grid[pll(5,9)].insert(4);
+    grid[pll(5,9)].insert(9);*/
+    grid[pll(3,4)].insert(5);
+    grid[pll(3,4)].insert(1);
+    grid[pll(3,4)].insert(2);
+    grid[pll(5,1)].insert(4);
+    grid[pll(5,1)].insert(8);
+    grid[pll(11,2)].insert(4);
+    grid[pll(11,2)].insert(9);
+    grid[pll(11,2)].insert(3);
+    grid[pll(12,5)].insert(11);
+    grid[pll(12,5)].insert(4);
+    grid[pll(12,5)].insert(8);
+    grid[pll(10,3)].insert(2);
+    grid[pll(10,3)].insert(7);
+    grid[pll(10,3)].insert(6);
+    grid[pll(1,2)].insert(1);
+    grid[pll(1,2)].insert(4);
+    grid[pll(1,2)].insert(6);
+    grid[pll(3,5)].insert(3);
+    grid[pll(3,5)].insert(7);
+    grid[pll(3,5)].insert(2);
+    grid[pll(1,4)].insert(1);
+    grid[pll(1,4)].insert(5);
+    grid[pll(3,6)].insert(3);
+    grid[pll(3,6)].insert(2);
+    grid[pll(7,1)].insert(5);
+    grid[pll(7,1)].insert(8);
+    grid[pll(7,1)].insert(6);
+    grid[pll(2,8)].insert(5);
+    grid[pll(2,8)].insert(12);
+    grid[pll(3,8)].insert(2);
+    grid[pll(5,10)].insert(4);
+    grid[pll(5,10)].insert(10);
+    grid[pll(7,8)].insert(11);
+    grid[pll(7,8)].insert(6);
+    grid[pll(1,6)].insert(10);
+    grid[pll(1,6)].insert(1);
+    grid[pll(1,6)].insert(2);
+    grid[pll(5,9)].insert(4);
     grid[pll(5,9)].insert(9);
 
     //ColorSpanningSets( 6, grid, {1,5,7,6,11,12});
     for (std::map<pll,set<int> >::iterator it=grid.begin(); it!=grid.end(); ++it)
         key.push_back(it->first);
     //key.pop_back();
-    for(int i=0;i<key.size();i++){
+    /*for(int i=0;i<key.size();i++){
         cout<<key[i].first<<"  "<<key[i].second<<endl;
-    }
+    }*/
 
     int dd[6]={1,5,7,6,11,12};
     vector<int> d(dd,dd+6);
 
-    ColorSpanningSets( m,6,d);
+    ColorSpanningSets( m,8,d);
 
     cout<<"number of color spanning subset => "<<number<<endl;
 
